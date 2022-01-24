85aff1e09e34dc8a61e68a74ebc0d8b5596d0e97
Shiro Amurha <46139101+shiroamurha@users.noreply.github.com>
2022-01-24 15:58:08 -0300

    Add files via upload

---
+++ requests.js
@@ -0,0 +1,6 @@
+"/lol-matchmaking/v1/ready-check/accept"
+"/lol-champions/v1/owned-champions-minimal",    
+"/lol-matchmaking/v1/ready-check",              
+"/lol-matchmaking/v1/ready-check/accept",       
+"/lol-champ-select/v1/session",                 
+"/lol-champ-select/v1/session/actions"
