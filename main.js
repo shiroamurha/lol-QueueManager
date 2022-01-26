c5368f17c5e880fded13eab9f54f205bef1a9e41
Luiz Fernando <39151654+obrientatsuya@users.noreply.github.com>
2022-01-26 03:00:32 -0300

    interface elements
    elements were created, just need to code the stylesheet

---
+++ main.js
@@ -0,0 +1,21 @@
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 375,
+    height: 495,
+    resizable: false,
+  });
+  win.setSkipTaskbar(true);
+  win.setMenu(null);
+  win.loadFile('index.html');
+ 
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
