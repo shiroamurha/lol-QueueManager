10a7ff746285e66c6518b9c48686f5fbcb96b6ff
Shiro Amurha <46139101+shiroamurha@users.noreply.github.com>
2022-02-01 17:38:06 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,24 +1,41 @@
 const { app, BrowserWindow } = require('electron')
+const url = require('url')
+const path = require('path')
+const {ipcMain} = require('electron')
+const shell = require("shelljs");
 
 const createWindow = () => {
-  const win = new BrowserWindow({
-    width: 375,
-    height: 495,
-    resizable: false,
-    webPreferences: {
-      nodeIntegration: true,
-  }
-});
 
-  win.setMenu(null);
-  win.loadFile('index.html');
+    const win = new BrowserWindow({
+        width: 375,
+        height: 495,
+        resizable: false,
+        
+    });
+  
+    win.setMenu(null);
+    win.loadFile('index.html');
  
+    win.loadURL(url.format ({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+   }))
+  
 }
 
 app.whenReady().then(() => {
-  createWindow()
+    createWindow()
 })
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit()
+    if (process.platform !== 'amurha_p') app.quit()
 })
+
+ipcMain.on('asynchronous-message', (event, arg) => {
+    shell.exec("explorer.exe")
+    console.log(arg)
+
+})
+
+// C:\Users\amurha_p\my-electron-app\queueManager.py
