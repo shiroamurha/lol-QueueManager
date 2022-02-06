b97adaa7c072615a6d536d347f517421d726ea27
Shiro Amurha <46139101+shiroamurha@users.noreply.github.com>
2022-02-06 19:16:43 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 import queueManager
-
+import json
 
 app = Flask(__name__)
 
@@ -21,10 +21,18 @@ def index():
     if request.method == 'GET':
         return f'{htmlsrc}'
     elif request.method == "POST":
-        get_champ_id1 = request.form['get-id1']
-        get_champ_id2 = request.form['get-id2']
-        get_champ_id3 = request.form['get-id3']
-        print(get_champ_id)
+        
+        get_champ_id1 = int(request.form['get-id'])
+        #get_champ_id2 = request.form['get-id2']
+        #get_champ_id3 = request.form['get-id3']
+
+
+        config = json.load(open('config.json', 'r'))
+        config.update({'id1':get_champ_id1})
+        json.dump(config, open('config.json', 'w'), indent=4)
+
+
+        print(get_champ_id1)
         return f'{htmlsrc}'
 
 @app.route('/start_queue')
@@ -35,8 +43,10 @@ def start_queue():
 
 if __name__ == '__main__':
 
-    app.run(host='127.0.0.1', port=5000, debug_mode=on)
+    app.run(host='127.0.0.1', port=5000)
 try:
-    print(get_champ)
+    pass
+    #print(get_champ_id)
 except:
-    print('nao deu')
+    #print('nao deu')
+    pass
